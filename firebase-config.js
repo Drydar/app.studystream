da60df06dab4f70122733673750b9db0929a5fd0
Drydar <d.rydar001@gmail.com>
2025-02-23 19:10:14 +0100

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,24 @@
-  // Import the functions you need from the SDKs you need
+
+  // Import Firebase modules
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
+  import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
+  import { getDatabase, ref, set, get } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-database.js";
+// Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
   // Your web app's Firebase configuration
   const firebaseConfig = {
-    apiKey: "AIzaSyDh6tdwKOwOvVVqUUZan_T3EP-Ssx30JLY",
-    authDomain: "streamplex-4fb97.firebaseapp.com",
-    projectId: "streamplex-4fb97",
-    storageBucket: "streamplex-4fb97.firebasestorage.app",
-    messagingSenderId: "163422250296",
-    appId: "1:163422250296:web:4f6a3fba1cd91f2a9f4dcd"
+    apiKey: "AIzaSyC5qQdiJpCHW6FtZnpa2RYdL-b66VqYQfk",
+    authDomain: "streamplex-935ff.firebaseapp.com",
+    projectId: "streamplex-935ff",
+    storageBucket: "streamplex-935ff.firebasestorage.app",
+    messagingSenderId: "32314467540",
+    appId: "1:32314467540:web:8a6ada369a64c4ecd80741"
   };
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
+  const auth = getAuth();
+  const database = getDatabase();
