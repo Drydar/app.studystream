59da34c2e01d7372b996b699a9a58b2e346d6d38
Drydar <d.rydar001@gmail.com>
2025-02-23 18:57:45 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDh6tdwKOwOvVVqUUZan_T3EP-Ssx30JLY",
+    authDomain: "streamplex-4fb97.firebaseapp.com",
+    projectId: "streamplex-4fb97",
+    storageBucket: "streamplex-4fb97.firebasestorage.app",
+    messagingSenderId: "163422250296",
+    appId: "1:163422250296:web:4f6a3fba1cd91f2a9f4dcd"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
